665df7ba58d37d493a2e095c8f31113f9d074b83
angelayang9483 <angelayang9483@gmail.com>
2025-05-29 01:33:04 -0700

    added login and signup authentication

--- app/(tabs)/login.jsx
+++ app/(tabs)/login.jsx
@@ -1,9 +1,57 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { useRouter } from 'expo-router';
+import { useState, useContext } from 'react';
+import { Pressable, StyleSheet, Text, TextInput, View, Alert } from 'react-native';
+import { AuthContext } from '../context/AuthContext';
+import * as SecureStore from 'expo-secure-store';
+import axios from 'axios';
+import config from '../config';
 
 export default function Tab() {
+  const url = config.BASE_URL;
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async () => {
+    if ( !username || !password ) {
+      Alert.alert("Error", "Please fill out all the fields.");
+      return false;
+    } else {
+      try {
+        const response = await axios.post(`${url}/api/users/login`, { username: username, password: password });
+        console.log("Login Success");
+        console.log(response.data);
+        return true;
+      }
+      catch (error) {
+        console.log("error", error);
+        console.log(error.response.data);
+      }
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text>Login Page</Text>
+
+      <Text>Username</Text>
+      <TextInput
+        style={styles.input}
+        value={username}
+        onChangeText={(text) => setUsername(text)}
+        placeholder="Enter your username"
+      />
+
+      <Text>Password</Text>
+      <TextInput
+        style={styles.input}
+        value={password}
+        secureTextEntry
+        onChangeText={(text) => setPassword(text)}
+        placeholder="Enter your password"
+      />
+
+      <Pressable onPress={handleSubmit}><Text>Log In</Text></Pressable>
     </View>
   );
 }
