9101936bfd7d13db41ff822d53244dfc9fc23f1c
melissa-guo <melissag2024@gmail.com>
2025-05-30 01:48:05 -0700

    forgot about frontend changes

--- app/config.js
+++ app/config.js
@@ -1,4 +1,4 @@
 const config = {
-  BASE_URL: 'https://de14-2607-f010-2a7-103f-dd1c-6272-c239-ea6a.ngrok-free.app'
+  BASE_URL: 'https://5e69-2607-f010-2a7-103f-dd1c-6272-c239-ea6a.ngrok-free.app'
 }
 export default config
