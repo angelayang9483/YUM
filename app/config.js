d8f70da41b7fa6abbd3cbb643c4aeb4e39b7303a
angelayang9483 <angelayang9483@gmail.com>
2025-06-03 03:11:04 -0700

    merge w main

--- app/config.js
+++ app/config.js
@@ -1,4 +1,4 @@
 const config = {
-  BASE_URL: 'hhttps://7728-2607-f010-2a7-103f-b43a-c64b-512d-b79e.ngrok-free.app'
+  BASE_URL: 'https://1ae6-2607-f010-2a7-1021-5512-af56-4514-6990.ngrok-free.app'
 }
 export default config
