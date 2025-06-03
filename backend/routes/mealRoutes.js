3ba29ab6d46b947874ca3325723687f6d9d2ab00
donnndo <donndo.06@gmail.com>
2025-06-02 23:50:40 -0700

    Added meal routes and controller

---
+++ backend/routes/mealRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const mealController = require('../controllers/mealController');
+
+// GET all meals
+router.get('/', mealController.getMeals);
+
+// GET meal by ID
+router.get('/:id', mealController.getMealById);
+
+// GET meal by name (search)
+router.get('/search', mealController.getMealsByName);
+
+// GET meals by category
+router.get('/category', mealController.getMealsByCategory);
+
+// POST new meal
+router.post('/', mealController.createMeal);
+
+module.exports = router;
