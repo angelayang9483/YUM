a153762dcc1eafd7389299f0ff92997821a009dc
Melissa Shi <melissaqshi@gmail.com>
2025-06-04 02:49:56 -0700

    added styling to profile and the food truck component

--- app/config.js
+++ app/config.js
@@ -1,4 +1,4 @@
 const config = {
-  BASE_URL: 'https://8b32-2607-f010-2a7-103f-8ff-65c-1d97-bf75.ngrok-free.app'
+  BASE_URL: 'https://6233-2607-f010-2a7-102d-3451-895d-e1e1-fc02.ngrok-free.app'
 }
 export default config
